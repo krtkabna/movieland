a6fe14befb687e1eb4fdf110fdebccae7774fc31
wasp <wasp@rocke.tech>
2022-03-31 18:59:30 +0200

    initial commit

---
+++ src/main/java/com/wasp/rottenpotatoes/RottenpotatoesApplication.java
@@ -0,0 +1,13 @@
+package com.wasp.rottenpotatoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RottenpotatoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RottenpotatoesApplication.class, args);
+	}
+
+}
