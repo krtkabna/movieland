d0638881bb156a6449b34dd76637baeef2db6379
wasp <wasp@rocke.tech>
2022-04-04 14:18:42 +0200

    use HikariDataSource

---
+++ src/main/java/com/wasp/rottenpotatoes/dao/config/JasyptConfig.java
@@ -0,0 +1,10 @@
+package com.wasp.rottenpotatoes.dao.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+}
