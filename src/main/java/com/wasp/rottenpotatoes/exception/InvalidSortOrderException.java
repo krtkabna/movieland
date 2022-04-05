0ef81187ad955b21f89aa247f250e1f5e8e3e31a
wasp <wasp@rocke.tech>
2022-04-05 23:22:55 +0200

    remove unused exception handler

--- src/main/java/com/wasp/rottenpotatoes/exception/InvalidSortOrderException.java
+++ src/main/java/com/wasp/rottenpotatoes/exception/InvalidSortOrderException.java
@@ -3,7 +3,7 @@ package com.wasp.rottenpotatoes.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST) //TODO ask which exception handler fits better
+@ResponseStatus(HttpStatus.BAD_REQUEST) //TODO ask which exception handler fits better (I think this one because json)
 public class InvalidSortOrderException extends RuntimeException {
     public InvalidSortOrderException(String message) {
         super(message);
